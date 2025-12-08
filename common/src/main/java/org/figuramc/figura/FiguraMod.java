ad15d977db938111c32e23ebfc0a5c6c359d0edd
llmmcc <97648020+llmmcc@users.noreply.github.com>
2025-12-08 22:14:05 +0800

    Update FiguraMod.java

--- common/src/main/java/org/figuramc/figura/FiguraMod.java
+++ common/src/main/java/org/figuramc/figura/FiguraMod.java
@@ -18,6 +18,7 @@ import org.figuramc.figura.compat.SimpleVCCompat;
 import org.figuramc.figura.config.Configs;
 import org.figuramc.figura.entries.EntryPointManager;
 import org.figuramc.figura.font.Emojis;
+import org.figuramc.figura.gui.widgets.lists.AvatarList;
 import org.figuramc.figura.lua.FiguraLuaPrinter;
 import org.figuramc.figura.lua.docs.FiguraDocsManager;
 import org.figuramc.figura.mixin.SkullBlockEntityAccessor;
