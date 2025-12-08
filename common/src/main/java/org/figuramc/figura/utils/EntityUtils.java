6ca4fba0368e24f742abcb0aec5bdd3308de6925
llmmcc <97648020+llmmcc@users.noreply.github.com>
2025-12-08 22:02:22 +0800

    Update EntityUtils.java

--- common/src/main/java/org/figuramc/figura/utils/EntityUtils.java
+++ common/src/main/java/org/figuramc/figura/utils/EntityUtils.java
@@ -90,7 +90,7 @@ public class EntityUtils {
     }
 
     public static boolean checkInvalidPlayer(UUID id) {
-        if (id.version() != 4)
+     if (id.version() < 3)
             return true;
 
         PlayerInfo playerInfo = getPlayerInfo(id);
