75b63902c27bd9d823a476f8c3671f81adb846ab
llmmcc <97648020+llmmcc@users.noreply.github.com>
2025-12-08 22:15:49 +0800

    Update ClientPacketListenerMixin.java

--- common/src/main/java/org/figuramc/figura/mixin/ClientPacketListenerMixin.java
+++ common/src/main/java/org/figuramc/figura/mixin/ClientPacketListenerMixin.java
@@ -50,17 +50,4 @@ public abstract class ClientPacketListenerMixin {
         
     }
     
-    @Inject(method = "handleUnknownCustomPayload", at = @At(value = "HEAD"), cancellable = true)
-    private void handleUnknownCustomPayload(CustomPacketPayload payload, CallbackInfo ci) {
-        if (payload.id().equals(FiguraMod.resReconnect)) {
-            ci.cancel();
-            AvatarManager.clearAvatars(FiguraMod.getLocalPlayerUUID());
-            try {
-                LocalAvatarLoader.loadAvatar(null, null);
-            } catch (Exception ignored) {}
-            AvatarManager.localUploaded = true;
-            AvatarList.selectedEntry = null;
-            NetworkStuff.auth();
-        }
-    }
 }
