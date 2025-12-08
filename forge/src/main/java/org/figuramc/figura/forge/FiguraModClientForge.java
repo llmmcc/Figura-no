b44a4e9defdc61f4b020c501d2884a98a419f99d
llmmcc <97648020+llmmcc@users.noreply.github.com>
2025-12-08 22:23:36 +0800

    Update FiguraModClientForge.java

--- forge/src/main/java/org/figuramc/figura/forge/FiguraModClientForge.java
+++ forge/src/main/java/org/figuramc/figura/forge/FiguraModClientForge.java
@@ -14,6 +14,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.network.ChannelBuilder;
 import net.minecraftforge.network.NetworkDirection;
 import net.minecraftforge.network.NetworkEvent;
 import net.minecraftforge.network.event.EventNetworkChannel;
@@ -48,6 +49,9 @@ public class FiguraModClientForge extends FiguraMod {
         for (VanillaGuiOverlay overlay : VanillaGuiOverlay.values()) {
             vanillaOverlays.add(overlay.type());
         }
+          ChannelBuilder.named(FiguraMod.resReconnect)
+                .eventNetworkChannel()
+                .addListener(e -> FiguraMod.reconnect());
     }
 
     @SubscribeEvent
